b75d9d48e1a405b0db402f9e2c9cdf1cab9fd557
Abduaws <www.abduaws@gmail.com>
2021-11-02 21:25:32 +0200

    update 3.0

---
+++ src/app/pages/mypocket/mypocket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mypocket',
+  templateUrl: './mypocket.component.html',
+  styleUrls: ['./mypocket.component.css']
+})
+export class MypocketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { ReminderComponent } from './reminder/reminder.component';
+import { MypocketComponent } from './mypocket/mypocket.component';
+import { ArchiveComponent } from './archive/archive.component';
+import { UseraccountComponent } from './useraccount/useraccount.component';
+import { TaskviewComponent } from './taskview/taskview.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { CreatetaskComponent } from './createtask/createtask.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    TasksComponent,
+    ReminderComponent,
+    MypocketComponent,
+    ArchiveComponent,
+    UseraccountComponent,
+    TaskviewComponent,
+    SigninComponent,
+    SignupComponent,
+    CreatetaskComponent
+  ],
   imports: [
     CommonModule
   ]
