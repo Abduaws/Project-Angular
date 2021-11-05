03422c51e8b6ddf6a06b9a0f9d90dbbfdab2315d
Abduaws <www.abduaws@gmail.com>
2021-11-05 18:40:55 +0200

    final?

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -12,7 +12,23 @@ export class SigninComponent implements OnInit{
     constructor (public __pageService: PageServiceService) { }
     
      ngOnInit(){
-       console.log(PageServiceService.loggedin)
+      // RUN ONLY ONCE TO INITIALIZE LOCAL DATABASE
+      // for(let i =0;i<10000;i++){
+      //     if(localStorage[i]==undefined){
+      //       localStorage.setItem(String(i),"Do Chores,01/01/2002,Default task")
+      //       localStorage.setItem(String(i+1),"Cook Food,01/01/2002,Default task")
+      //       localStorage.setItem(String(i+2),"Study,01/01/2002,Default task")
+      //       localStorage.setItem(String(i+3),"Take dog for a walk,01/01/2002,Default task")
+      //       localStorage.setItem(String(i+4),"Go to sleep,01/01/2002,Default task")
+      //       localStorage.setItem(String(i+5),"Play,01/01/2002,Default task")
+      //       localStorage.setItem(String(i+6),"Program,01/01/2002,Default task")
+      //       localStorage.setItem(String(i+7),"Solve assignment,01/01/2002,Default task")
+      //       localStorage.setItem(String(i+8),"Eat food,01/01/2002,Default task")
+      //       localStorage.setItem(String(i+9),"Kill yourself,01/01/2002,Default task")
+      //       break
+      //     }
+      //   }
+      console.log(PageServiceService.loggedin)
       if(SigninComponent.alreadydone){
         return 0;
       }
